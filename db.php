60251a4c27eeddaec5bd934171c4f8e87c778091
VARSHA RANA <159460240+ranavarsha@users.noreply.github.com>
2025-12-29 14:03:30 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $conn = new mysqli(
     "127.0.0.1",          // host
     "inventrack_user",    // username
-    "1234",          // password (use your actual password)
+    "",          // password (use your actual password)
     "inventrack",         // database
     3307                  // 🔥 IMPORTANT: MySQL PORT
 );
@@ -11,3 +11,4 @@ if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 ?>
+
