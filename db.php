e79674fe806388172696c3492c8a122b0c1162a4
VARSHA RANA <159460240+ranavarsha@users.noreply.github.com>
2026-01-03 22:51:40 +0530

    Refactor DB connection to use environment variables
    Updated database connection to use environment variables for configuration.

--- db.php
+++ db.php
@@ -1,14 +1,13 @@
 <?php
-$conn = new mysqli(
-    "127.0.0.1",          // host
-    "inventrack_user",    // username
-    "",          // password (use your actual password)
-    "inventrack",         // database
-    3307                  // 🔥 IMPORTANT: MySQL PORT
-);
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+    die("DB Connection Failed");
 }
 ?>
-
