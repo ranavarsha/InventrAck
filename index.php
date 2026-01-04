5741484f0817e6657f5d1b71ea916b872e8a4b5a
VARSHA RANA <159460240+ranavarsha@users.noreply.github.com>
2026-01-04 10:36:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo "Inventrack is LIVE on Railway 🚀";
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+} else {
+    header("Location: list_products.php");
+    exit();
+}
+?>
